10a627aa25dd2d15e67c6d326ef9586640b1cdd2
刘胜情 <308393896@qq.com>
2016-04-10 18:32:40 +0800

    test

--- WebPrinter/src/com/webprinter/dao/UserDao.java
+++ WebPrinter/src/com/webprinter/dao/UserDao.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import com.webprinter.entity.User;
 
+/**
+ * 
+ * @author apple
+ *
+ */
 public interface UserDao {
 	public boolean addUser(User user);
 	public User findUserByUserName(String userName);
